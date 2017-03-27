6cc3d2eb2c4ad7cec3c35d4cd46559d9b1063695
goenning <me@goenning.net>
2017-03-27 20:04:55 +0100

    more pg tests

--- app/postgres/feedback_test.go
+++ app/postgres/feedback_test.go
@@ -35,3 +35,60 @@ func TestIdeaService_GetAll(t *testing.T) {
 	Expect(ideas[1].Description).To(Equal("Description #2"))
 	Expect(ideas[1].User.Name).To(Equal("Arya Start"))
 }
+
+func TestIdeaService_SaveAndGet(t *testing.T) {
+	RegisterTestingT(t)
+	db := setup()
+	defer teardown(db)
+
+	execute(db, "INSERT INTO tenants (name, subdomain, created_on) VALUES ('My Domain Inc.','mydomain', now())")
+	execute(db, "INSERT INTO users (name, email, created_on) VALUES ('Jon Snow','jon.snow@got.com', now())")
+
+	svc := &postgres.IdeaService{DB: db}
+	idea, err := svc.Save(1, 1, "My new idea", "with this description")
+	Expect(err).To(BeNil())
+	Expect(idea.ID).To(Equal(1))
+
+	dbIdea, err := svc.GetByID(1, 1)
+
+	Expect(err).To(BeNil())
+	Expect(dbIdea.ID).To(Equal(1))
+	Expect(dbIdea.Title).To(Equal("My new idea"))
+	Expect(dbIdea.Description).To(Equal("with this description"))
+	Expect(dbIdea.User.ID).To(Equal(1))
+	Expect(dbIdea.User.Name).To(Equal("Jon Snow"))
+	Expect(dbIdea.User.Email).To(Equal("jon.snow@got.com"))
+}
+
+func TestIdeaService_GetInvalid(t *testing.T) {
+	RegisterTestingT(t)
+	db := setup()
+	defer teardown(db)
+
+	svc := &postgres.IdeaService{DB: db}
+	dbIdea, err := svc.GetByID(1, 1)
+
+	Expect(err).ToNot(BeNil())
+	Expect(dbIdea).To(BeNil())
+}
+
+func TestIdeaService_AddAndReturnComments(t *testing.T) {
+	RegisterTestingT(t)
+	db := setup()
+	defer teardown(db)
+
+	execute(db, "INSERT INTO tenants (name, subdomain, created_on) VALUES ('My Domain Inc.','mydomain', now())")
+	execute(db, "INSERT INTO users (name, email, created_on) VALUES ('Jon Snow','jon.snow@got.com', now())")
+
+	svc := &postgres.IdeaService{DB: db}
+	idea, _ := svc.Save(1, 1, "My new idea", "with this description")
+	svc.AddComment(1, idea.ID, "Comment #1")
+	svc.AddComment(1, idea.ID, "Comment #2")
+
+	comments, err := svc.GetCommentsByIdeaID(1, idea.ID)
+	Expect(err).To(BeNil())
+	Expect(len(comments)).To(Equal(2))
+
+	Expect(comments[0].Content).To(Equal("Comment #2"))
+	Expect(comments[1].Content).To(Equal("Comment #1"))
+}
