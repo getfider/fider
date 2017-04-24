08764d35e3ee892ec4a1b3de04e01f25a88f2fe6
goenning <me@goenning.net>
2017-04-24 07:55:11 +0100

    remove unused

--- cover.sh
+++ cover.sh
@@ -10,19 +10,6 @@ if [[ -d "$COVER" ]]; then
 fi
 mkdir -p "$COVER"
 
-# If a package directory has a .nocover file, don't count it when calculating
-# coverage.
-filter=""
-for pkg in "$@"; do
-	if [[ -f "$GOPATH/src/$pkg/.nocover" ]]; then
-		if [[ -n "$filter" ]]; then
-			filter="$filter, "
-		fi
-		filter="\"$pkg\": true"
-	fi
-done
-
-
 i=0
 for pkg in "$@"; do
 	i=$((i + 1))
@@ -40,7 +27,6 @@ for pkg in "$@"; do
 		| map
 			( select(startswith("'"$ROOT_PKG"'"))
 			| select(contains("/vendor/") | not)
-			| select(in({'"$filter"'}) | not)
 			)
 		| join(",")
 	')
@@ -50,7 +36,6 @@ for pkg in "$@"; do
 		| map
 			( select(startswith("'"$ROOT_PKG"'"))
 			| select(contains("/vendor/") | not)
-			| select(in({'"$filter"'}) | not)
 			)
 		| join(",")
 	')
