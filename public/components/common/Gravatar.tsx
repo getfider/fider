b35c52d6ee50c624979e216c46e17ec4481cb22f
goenning <me@goenning.net>
2017-08-16 18:40:17 +0100

    use letters as default

--- public/components/common/Gravatar.tsx
+++ public/components/common/Gravatar.tsx
@@ -10,12 +10,13 @@ interface GravatarProps {
 
 export const Gravatar = (props: GravatarProps) => {
   let element: any;
-  const error = () => {
-    element.src = `${getBaseUrl()}/avatars/50/${props.name}`;
-  };
-  const hash = props.email ? md5(props.email) : props.hash || '';
+  const fallback = `${getBaseUrl()}/avatars/50/${props.name}`;
 
+  const hash = props.email ? md5(props.email) : props.hash || '';
   return (hash && props.name) ?
-    <img ref={(e) => element = e} onError={error} className="ui avatar image" src={ `https://www.gravatar.com/avatar/${hash}?d=404` }/> : 
+    <img ref={(e) => element = e }
+         onError={() => { element.src = fallback; }}
+         className="ui avatar image"
+         src={ `https://www.gravatar.com/avatar/${hash}?d=${fallback}` }/> :
     <div />;
 };
