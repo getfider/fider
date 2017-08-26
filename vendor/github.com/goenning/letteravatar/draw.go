a1b5c5be0872f15859332a909ec6ba3f9858e4ce
goenning <me@goenning.net>
2017-08-26 20:36:42 +0100

    update letteravatar dependencies

--- vendor/github.com/goenning/letteravatar/draw.go
+++ vendor/github.com/goenning/letteravatar/draw.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 	"sync"
 	"time"
+	"unicode"
 	"unicode/utf8"
 
 	"github.com/golang/freetype"
@@ -32,11 +33,21 @@ var defaultLetterColor = color.RGBA{0xf0, 0xf0, 0xf0, 0xf0}
 
 // Extract two letters from given name
 func Extract(name string) string {
-	slices := strings.FieldsFunc(name, split)
-	if len(slices) >= 2 {
-		return first(slices[0]) + first(slices[1])
+	name = strings.Trim(name, " ")
+	if name == "" {
+		return "?"
 	}
-	return first(name)
+
+	slices := splitBySpace(name)
+	if len(slices) < 2 {
+		slices = splitByCamelCase(name)
+	}
+
+	if len(slices) >= 2 {
+		return strings.ToUpper(first(slices[0]) + first(slices[1]))
+	}
+
+	return strings.ToUpper(first(name))
 }
 
 func first(s string) string {
@@ -44,8 +55,55 @@ func first(s string) string {
 	return string(firstLetter)
 }
 
-func split(r rune) bool {
-	return r == '.' || r == ',' || r == ' '
+func splitBySpace(src string) (entries []string) {
+	return strings.FieldsFunc(src, func(r rune) bool {
+		return r == '.' || r == ',' || r == ' '
+	})
+}
+
+func splitByCamelCase(src string) (entries []string) {
+	// don't split invalid utf8
+	if !utf8.ValidString(src) {
+		return []string{src}
+	}
+	entries = []string{}
+	var runes [][]rune
+	lastClass := 0
+	class := 0
+	// split into fields based on class of unicode character
+	for _, r := range src {
+		switch true {
+		case unicode.IsLower(r):
+			class = 1
+		case unicode.IsUpper(r):
+			class = 2
+		case unicode.IsDigit(r):
+			class = 3
+		default:
+			class = 4
+		}
+		if class == lastClass {
+			runes[len(runes)-1] = append(runes[len(runes)-1], r)
+		} else {
+			runes = append(runes, []rune{r})
+		}
+		lastClass = class
+	}
+	// handle upper case -> lower case sequences, e.g.
+	// "PDFL", "oader" -> "PDF", "Loader"
+	for i := 0; i < len(runes)-1; i++ {
+		if unicode.IsUpper(runes[i][0]) && unicode.IsLower(runes[i+1][0]) {
+			runes[i+1] = append([]rune{runes[i][len(runes[i])-1]}, runes[i+1]...)
+			runes[i] = runes[i][:len(runes[i])-1]
+		}
+	}
+	// construct []string from results
+	for _, s := range runes {
+		if len(s) > 0 {
+			entries = append(entries, string(s))
+		}
+	}
+	return
 }
 
 // Draw generates a new letter-avatar image of the given size using the given letter
