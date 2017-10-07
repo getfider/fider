2e9181ffa385cb89e25475af56910328c0f4b9c8
goenning <me@goenning.net>
2017-10-07 12:30:31 +0100

    clean up

--- cover.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo 'mode: atomic' > cover.out && go list ./... | xargs -n1 -I{} sh -c 'go test -covermode=atomic -coverpkg=./... -coverprofile=coverage.tmp {} && tail -n +2 coverage.tmp >> cover.out' && rm coverage.tmp
