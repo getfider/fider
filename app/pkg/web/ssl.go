8eb8648e6455cde8c9bcfe5a526ae0d1fe949299
Guilherme Oenning <me@goenning.net>
2017-10-27 18:40:56 +0100

    use configured SSL if compatible on AutoSSL mode (#153)

---
+++ app/pkg/web/ssl.go
@@ -0,0 +1,52 @@
+package web
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+
+	"golang.org/x/crypto/acme/autocert"
+)
+
+//CertificateManager is used to manage SSL certificates
+type CertificateManager struct {
+	cert    tls.Certificate
+	leaf    *x509.Certificate
+	autossl autocert.Manager
+}
+
+//NewCertificateManager creates a new CertificateManager
+func NewCertificateManager(certFile, keyFile, cacheDir string) (*CertificateManager, error) {
+	manager := &CertificateManager{
+		autossl: autocert.Manager{
+			Prompt: autocert.AcceptTOS,
+			Cache:  autocert.DirCache(cacheDir),
+		},
+	}
+
+	if certFile != "" && keyFile != "" {
+		var err error
+		manager.cert, err = tls.LoadX509KeyPair(certFile, keyFile)
+		if err != nil {
+			return nil, err
+		}
+
+		manager.leaf, err = x509.ParseCertificate(manager.cert.Certificate[0])
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return manager, nil
+}
+
+//GetCertificate decides which certificate to use
+//It first tries to use loaded certificate for incoming request if it's compatible
+//Otherwise fallsback to a automatically generated certificate by Let's Encrypt
+func (m *CertificateManager) GetCertificate(hello *tls.ClientHelloInfo) (*tls.Certificate, error) {
+	if m.leaf != nil {
+		if m.leaf.VerifyHostname(hello.ServerName) != nil {
+			return &m.cert, nil
+		}
+	}
+	return m.autossl.GetCertificate(hello)
+}
