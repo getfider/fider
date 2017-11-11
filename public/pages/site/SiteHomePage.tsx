037e84e38adb375207b53e4fb952fb57929d028c
Guilherme Oenning <me@goenning.net>
2017-11-11 10:09:50 +0000

    add search field (#177)

--- public/pages/site/SiteHomePage.tsx
+++ public/pages/site/SiteHomePage.tsx
@@ -8,6 +8,7 @@ import { IdeaFilter, IdeaFilterFunction } from '@fider/components/IdeaFilter';
 
 import { inject, injectables } from '@fider/di';
 import { Session } from '@fider/services';
+import { getBaseUrl, getQueryString } from '@fider/utils/page';
 
 import './SiteHomePage.scss';
 
@@ -16,7 +17,17 @@ const defaultShowCount = 20;
 interface SiteHomePageState {
   ideas: Idea[];
   showCount: number;
+  searching: boolean;
+  search?: string;
+  activeFilter: string;
 }
+
+const EmptyList = () => {
+  return <div className="center">
+    <p><i className="icon lightbulb" aria-hidden="true"></i></p>
+    <p>It's lonely out here. Start by sharing an idea!</p>
+  </div>;
+};
 
 const ListIdeaItem = (props: { idea: Idea, user?: CurrentUser }) => {
   return <div className="item">
@@ -39,7 +50,6 @@ export class SiteHomePage extends React.Component<{}, SiteHomePageState> {
     private tenant: Tenant;
     private allIdeas: Idea[];
     private filter: HTMLDivElement;
-    private activeFilter: string;
 
     @inject(injectables.Session)
     public session: Session;
@@ -50,18 +60,57 @@ export class SiteHomePage extends React.Component<{}, SiteHomePageState> {
         this.tenant = this.session.getCurrentTenant();
         this.allIdeas = this.session.get<Idea[]>('ideas') || [];
 
-        this.activeFilter = window.location.hash.substring(1);
+        const search = getQueryString('q');
+        const activeFilter = window.location.hash.substring(1);
         this.state = {
-          ideas: IdeaFilter.getFilter(this.activeFilter)(this.allIdeas),
-          showCount: defaultShowCount
+          ideas: this.filterIdeas(activeFilter, search),
+          showCount: defaultShowCount,
+          activeFilter,
+          searching: !!search,
+          search
         };
     }
 
-    private filterChanged(name: string, filter: IdeaFilterFunction) {
-      window.location.hash = `#${name}`;
+    private containsAll(str: string, substrings: string[]): boolean {
+        for (let i = 0; i !== substrings.length; i++) {
+           if (str.indexOf(substrings[i]) === - 1) {
+             return false;
+           }
+        }
+        return true;
+    }
+
+    private filterIdeas(activeFilter: string, search?: string): Idea[] {
+      let newUrl = getBaseUrl();
+      let ideas = [];
+
+      if (search) {
+        const s = search.trim().toLowerCase();
+        newUrl += `?q=${encodeURIComponent(s).replace(/%20/g, '+')}`;
+        ideas = this.allIdeas.filter((idea) => {
+          const terms = s.split(' ').filter((x) => x.length >= 2);
+          return (
+            this.containsAll(idea.title.toLowerCase(), terms) ||
+            this.containsAll(idea.description.toLowerCase(), terms) ||
+            (idea.response && this.containsAll(idea.response.text.toLowerCase(), terms))
+          );
+        });
+      } else {
+        newUrl += activeFilter ? `#${activeFilter}` : '';
+        ideas = IdeaFilter.getFilter(activeFilter)(this.allIdeas);
+      }
+
+      if (history.replaceState) {
+        window.history.replaceState({ path: newUrl }, '', newUrl);
+      }
+      return ideas;
+    }
+
+    private filterChanged(name: string) {
       this.setState({
-        ideas: filter(this.allIdeas),
-        showCount: defaultShowCount
+        ideas: this.filterIdeas(name, this.state.search),
+        showCount: defaultShowCount,
+        activeFilter: name,
       });
     }
 
@@ -69,6 +118,21 @@ export class SiteHomePage extends React.Component<{}, SiteHomePageState> {
       event.preventDefault();
       this.setState({
         showCount: this.state.showCount + defaultShowCount
+      });
+    }
+
+    private resetSearch() {
+      this.setState({
+        search: '',
+        searching: false,
+        ideas: this.filterIdeas(this.state.activeFilter, ''),
+      });
+    }
+
+    private searchIdea(input: string): void {
+      this.setState({
+        search: input,
+        ideas: this.filterIdeas(this.state.activeFilter, input)
       });
     }
 
@@ -87,7 +151,7 @@ export class SiteHomePage extends React.Component<{}, SiteHomePageState> {
                 </h5>
               }
           </div>
-          : <p>No ideas found for given filter.</p>;
+          : <p className="no-ideas-found">No ideas found for given filter.</p>;
 
         const welcomeMessage = this.tenant.welcomeMessage ||
         `## Welcome to our feedback forum!
@@ -106,12 +170,31 @@ We'd love to hear what you're thinking about. What can we do better? This is the
                       <div className="ten wide column">
                         {
                           this.allIdeas.length === 0
-                          ? <div className="center">
-                              <p><i className="icon lightbulb" aria-hidden="true"></i></p>
-                              <p>It's lonely out here. Start by sharing an idea!</p>
-                            </div>
+                          ? <EmptyList />
                           : <div>
-                              <IdeaFilter activeFilter={ this.activeFilter } filterChanged={ this.filterChanged.bind(this) } />
+                              <div className="ui grid">
+                                { !this.state.searching && <div className="ten wide mobile ten wide tablet twelve wide computer column filter-column">
+                                  <IdeaFilter activeFilter={ this.state.activeFilter } filterChanged={ this.filterChanged.bind(this) } />
+                                </div> }
+                                <div className={ !this.state.searching ? `six wide mobile six wide tablet four wide computer column` : 'column' }>
+                                  <div className="ui search">
+                                    <div className="ui icon fluid input">
+                                      <input
+                                        onFocus={ () => this.setState({ searching: true }) }
+                                        onBlur={ (x) => this.setState({ searching: !!this.state.search }) }
+                                        onChange={ (x) => this.searchIdea(x.currentTarget.value) }
+                                        value={ this.state.search }
+                                        type="text"
+                                        placeholder="Search..." />
+                                      {
+                                        this.state.searching
+                                        ? <i onClick={ () => this.resetSearch() } className="cancel link icon"></i>
+                                        : <i className="search icon"></i>
+                                      }
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
                               { displayIdeas }
                             </div>
                         }
