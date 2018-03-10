a2555cea219e9197e1a45738cdb6cccd4f21fd6c
Guilherme Oenning <me@goenning.net>
2018-03-10 12:24:24 +0000

    chore: update to 9.6.8 (#306)

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -17,7 +17,7 @@ run_e2e () {
 
   echo "Starting Fider (HOST_MODE: $1)..."
   docker rm -f $PG_CONTAINER || true
-  docker run -d -e POSTGRES_USER=fider_e2e -e POSTGRES_PASSWORD=fider_e2e_pw --name $PG_CONTAINER postgres:9.6.2
+  docker run -d -e POSTGRES_USER=fider_e2e -e POSTGRES_PASSWORD=fider_e2e_pw --name $PG_CONTAINER postgres:9.6.8
   docker run --link $PG_CONTAINER:pg waisbrot/wait
   docker run -d -p 3000:3000 --link $PG_CONTAINER -e HOST_MODE=$1 -e DATABASE_URL=postgres://fider_e2e:fider_e2e_pw@$PG_CONTAINER:5432/fider_e2e?sslmode=disable --env-file .env --name $FIDER_CONTAINER getfider/fider:e2e
 
