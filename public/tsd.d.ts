e66e2d2ec1cba05e6e45a832dff63a191476c06e
goenning <me@goenning.net>
2018-03-20 21:06:04 +0000

    remove jquery

--- public/tsd.d.ts
+++ public/tsd.d.ts
@@ -1,9 +1,3 @@
-interface JQuery {
-  modal(opts: any): any;
-  popup(args: any): any;
-  dropdown(args?: any, args2?: any): any;
-}
-
 interface Window {
   ga?: (cmd: string, evt: string, args?: any) => void;
 }
