20af40901ca9d489cd079069efb6185cd4be591c
goenning <me@goenning.net>
2018-03-24 21:01:52 +0000

    add new pkg/errors

---
+++ app/pkg/errors/errors.go
@@ -0,0 +1,54 @@
+package errors
+
+import (
+	stdError "errors"
+	"fmt"
+	"runtime"
+)
+
+type theError struct {
+	text  string
+	cause error
+}
+
+func caller() (string, int) {
+	_, file, line, _ := runtime.Caller(2)
+	file = trimBasePath(file)
+	return file, line
+}
+
+//New creates a new error
+func New(text string) error {
+	file, line := caller()
+	return stdError.New(fmt.Sprintf("%s (%s:%d)", text, file, line))
+}
+
+//Wrap existing error with additional text
+func Wrap(text string, err error) error {
+	file, line := caller()
+
+	casted, ok := err.(*theError)
+	if ok {
+		text = fmt.Sprintf("%s (%s:%d)\n- %s", text, file, line, casted.text)
+	} else {
+		text = fmt.Sprintf("%s (%s:%d)", text, file, line)
+	}
+	return &theError{
+		text:  text,
+		cause: Cause(err),
+	}
+}
+
+//Cause returns the original cause of the error stack
+func Cause(err error) error {
+	casted, ok := err.(*theError)
+	if ok {
+		return casted.cause
+	}
+	return err
+}
+
+//Error formats the error trace into human readable
+func (err *theError) Error() string {
+	return fmt.Sprintf("Error Trace: \n- %s\n- %v", err.text, err.cause)
+}
---
+++ app/pkg/errors/errors_test.go
@@ -0,0 +1,37 @@
+package errors_test
+
+import (
+	"testing"
+
+	"github.com/getfider/fider/app/pkg/errors"
+
+	. "github.com/onsi/gomega"
+)
+
+func TestNewError(t *testing.T) {
+	RegisterTestingT(t)
+
+	first := errors.New("document not found")
+	Expect(first.Error()).To(Equal("document not found (app/pkg/errors/errors_test.go:14)"))
+}
+
+func TestWrappedError(t *testing.T) {
+	RegisterTestingT(t)
+
+	first := errors.New("document not found")
+	wrapped := errors.Wrap("could not create user", first)
+	wrappedAgain := errors.Wrap("failed to register new user", wrapped)
+
+	Expect(first.Error()).To(Equal(`document not found (app/pkg/errors/errors_test.go:21)`))
+	Expect(wrapped.Error()).To(Equal(`Error Trace: 
+- could not create user (app/pkg/errors/errors_test.go:22)
+- document not found (app/pkg/errors/errors_test.go:21)`))
+	Expect(wrappedAgain.Error()).To(Equal(`Error Trace: 
+- failed to register new user (app/pkg/errors/errors_test.go:23)
+- could not create user (app/pkg/errors/errors_test.go:22)
+- document not found (app/pkg/errors/errors_test.go:21)`))
+
+	Expect(errors.Cause(wrapped)).To(Equal(first))
+	Expect(errors.Cause(first)).To(Equal(first))
+	Expect(errors.Cause(wrappedAgain)).To(Equal(first))
+}
