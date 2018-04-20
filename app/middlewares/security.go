16a72d44c67ba42563dad69a9b4dbc59646a15ab
Guilherme Oenning <me@goenning.net>
2018-04-20 07:46:53 +0100

    fix: allow use of Fider on frames (#354)

--- app/middlewares/security.go
+++ app/middlewares/security.go
@@ -12,7 +12,6 @@ func Secure() web.MiddlewareFunc {
 		return func(c web.Context) error {
 			c.Response.Header().Add("Content-Security-Policy-Report-Only", fmt.Sprintf(web.CspPolicyTemplate, c.ContextID()))
 			c.Response.Header().Add("X-XSS-Protection", "1; mode=block")
-			c.Response.Header().Add("X-Frame-Options", "DENY")
 			c.Response.Header().Add("X-Content-Type-Options", "nosniff")
 			c.Response.Header().Add("Referrer-Policy", "no-referrer-when-downgrade")
 			return next(c)
