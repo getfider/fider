b8f3639c9b622bc026b4d2274d53475c15c281b9
goenning <me@goenning.net>
2018-05-01 19:22:03 +0100

    listen to signals

--- app/cmd/server.go
+++ app/cmd/server.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
+	"os"
+	"os/signal"
+	"runtime/pprof"
+	"syscall"
 
 	"github.com/getfider/fider/app/models"
 	"github.com/getfider/fider/app/pkg/env"
@@ -12,11 +17,36 @@ import (
 //Returns an exitcode, 0 for OK and 1 for ERROR
 func RunServer(settings *models.SystemSettings) int {
 
+	e := routes(web.New(settings))
 	fmt.Printf("Application is starting...\n")
 	fmt.Printf("GO_ENV: %s\n", env.Current())
 
-	e := routes(web.New(settings))
+	listenSignals(e, settings)
 	e.Start(":" + env.GetEnvOrDefault("PORT", "3000"))
 
 	return 0
+}
+
+func listenSignals(e *web.Engine, settings *models.SystemSettings) {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGUSR1)
+	go func() {
+		for {
+			s := <-signals
+			switch s {
+			case syscall.SIGUSR1:
+				e.Logger().Infof("SIGUSR1 received")
+				e.Logger().Infof("Dumping process status")
+				buf := new(bytes.Buffer)
+				buf.WriteString(fmt.Sprintf("Version: %s\n", settings.Version))
+				buf.WriteString(fmt.Sprintf("BuildTime: %s\n", settings.BuildTime))
+				buf.WriteString(fmt.Sprintf("Compiler: %s\n", settings.Compiler))
+				buf.WriteString(fmt.Sprintf("Environment: %s\n", settings.Environment))
+				buf.WriteString(fmt.Sprintf("Worker Queue: %d\n", e.Worker().Length()))
+				pprof.Lookup("goroutine").WriteTo(buf, 1)
+				pprof.Lookup("heap").WriteTo(buf, 1)
+				e.Logger().Infof("%s", buf.String())
+			}
+		}
+	}()
 }
