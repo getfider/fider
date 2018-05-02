b8920a5a55cf6c2c2b62b42c28e946b6a606fd0a
goenning <me@goenning.net>
2018-05-02 20:20:01 +0100

    fix tests

--- app/pkg/web/engine_test.go
+++ app/pkg/web/engine_test.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/getfider/fider/app/models"
+
 	"github.com/getfider/fider/app/pkg/web"
 
 	. "github.com/onsi/gomega"
@@ -11,18 +13,24 @@ import (
 
 func TestEngine_StartRequestStop(t *testing.T) {
 	RegisterTestingT(t)
-	w := web.New(nil)
+	w := web.New(&models.SystemSettings{})
 	group := w.Group()
 	{
 		group.Get("/hello", func(c web.Context) error {
 			return c.Ok(web.Map{})
 		})
 	}
+
 	go w.Start(":8080")
 	resp, err := http.Get("http://localhost:8080/hello")
 	Expect(err).To(BeNil())
 	resp.Body.Close()
 	Expect(resp.StatusCode).To(Equal(http.StatusOK))
+
+	resp, err = http.Get("http://localhost:8080/world")
+	Expect(err).To(BeNil())
+	resp.Body.Close()
+	Expect(resp.StatusCode).To(Equal(http.StatusNotFound))
 
 	err = w.Stop()
 	Expect(err).To(BeNil())
