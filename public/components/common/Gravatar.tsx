97880aba83e2681739666288c269fc261d7ffe53
goenning <me@goenning.net>
2018-05-31 07:17:53 +0100

    better show response design

--- public/components/common/Gravatar.tsx
+++ public/components/common/Gravatar.tsx
@@ -6,15 +6,18 @@ import { User, UserRole, UserStatus, CurrentUser } from "@fider/models";
 
 interface GravatarProps {
   user?: User;
+  size?: "small" | "normal" | "large";
 }
 
 export const Gravatar = (props: GravatarProps) => {
+  const size = props.size || "normal";
   const id = props.user ? props.user.id : 0;
   const name = props.user ? props.user.name : "";
   const url = `${page.getBaseUrl()}/avatars/50/${id}/${encodeURIComponent(name || "?")}`;
 
   const className = classSet({
     "c-avatar": true,
+    [`m-${size}`]: true,
     "m-staff": props.user && props.user.role >= UserRole.Collaborator
   });
 
