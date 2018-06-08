7394b980d939ce6c208d574e4094f3be3e432744
goenning <me@goenning.net>
2018-06-08 07:14:26 +0100

    reorder

--- public/pages/Home/components/IdeaFilter.tsx
+++ public/pages/Home/components/IdeaFilter.tsx
@@ -20,8 +20,8 @@ export class IdeaFilter extends React.Component<IdeaFilterProps, {}> {
   public render() {
     const options: DropdownItemProps[] = [
       { text: "trending", value: "trending", content: "Trending" },
-      { text: "my votes", value: "my-votes", content: "My Votes" },
       { text: "recent", value: "recent", content: "Recent" },
+      { text: "my votes", value: "my-votes", content: "My Votes" },
       { text: "most wanted", value: "most-wanted", content: "Most Wanted" },
       { text: "most discussed", value: "most-discussed", content: "Most Discussed" }
     ];
