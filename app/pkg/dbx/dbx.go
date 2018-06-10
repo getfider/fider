getfider/fider

commit f69cfaad0c25728be6c6613b7429e2c8f2dabd5e

bugfix: turn on race condition flag and add mutex everywhere (#427)

app/pkg/dbx/mapping.go
@@ -3,6 +3,7 @@ package dbx
 import (
 	"fmt"
 	"reflect"
+	"sync"
 
 	"github.com/lib/pq"
 )
@@ -10,6 +11,7 @@ import (
 //RowMapper is resposible for mapping a sql.Rows into a Struct (model)
 type RowMapper struct {
 	cache map[reflect.Type]TypeMapper
+	sync.RWMutex
 }
 
 //NewRowMapper creates a new instance of RowMapper
@@ -31,9 +33,15 @@ func (m *RowMapper) Map(dest interface{}, columns []string, scanner func(dest ..
 		ok         bool
 	)
 
-	if typeMapper, ok = m.cache[t]; !ok {
+	m.RLock()
+	typeMapper, ok = m.cache[t]
+	m.RUnlock()
+
+	if !ok {
 		typeMapper = NewTypeMapper(t)
+		m.Lock()
 		m.cache[t] = typeMapper
+		m.Unlock()
 	}
 
 	pointers := make([]interface{}, len(columns))

Makefile
@@ -18,12 +18,12 @@ test-ui:
 	TZ='GMT' npx jest ./public
 
 test-server:
-	godotenv -f .test.env go test ./...
+	godotenv -f .test.env go test ./... -race
 
 test : test-server test-ui
 
 coverage:
-	godotenv -f .test.env go test ./... -coverprofile=cover.out -coverpkg=all
+	godotenv -f .test.env go test ./... -coverprofile=cover.out -coverpkg=all -race
 
 e2e-single:
 	./scripts/e2e.sh single

app/pkg/assert/assert_test.go
@@ -1,6 +1,7 @@
 package assert_test
 
 import (
+	"sync"
 	"testing"
 	"time"
 
@@ -115,14 +116,19 @@ func TestFuncPanics(t *testing.T) {
 func TestFuncEventuallyEquals(t *testing.T) {
 	mockT := new(testing.T)
 	RegisterT(mockT)
+	mu := &sync.RWMutex{}
 
 	completed := false
 	go func() {
 		time.Sleep(500 * time.Millisecond)
+		mu.Lock()
+		defer mu.Unlock()
 		completed = true
 	}()
 
 	if !Expect(func() bool {
+		mu.RLock()
+		defer mu.RUnlock()
 		return completed
 	}).EventuallyEquals(true) {
 		t.Error("EventuallyEquals assertion failed")

app/pkg/dbx/mapping_test.go
@@ -2,6 +2,7 @@ package dbx_test
 
 import (
 	"reflect"
+	"sync"
 	"testing"
 
 	. "github.com/getfider/fider/app/pkg/assert"
@@ -71,6 +72,21 @@ func TestRowMapper_NonStruct(t *testing.T) {
 	var id int
 	mapper.Map(&id, []string{"id"}, createScanner(5))
 	Expect(id).Equals(5)
+}
+
+func TestRowMapper_Concurrent(t *testing.T) {
+	RegisterT(t)
+	var wg sync.WaitGroup
+	mapper := dbx.NewRowMapper()
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func() {
+			var id int
+			mapper.Map(&id, []string{"id"}, createScanner(5))
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }
 
 func TestRowMapper_SimpleStruct(t *testing.T) {

app/pkg/worker/worker.go
@@ -3,7 +3,7 @@ package worker
 import (
 	"context"
 	"errors"
-	"sync/atomic"
+	"sync"
 	"time"
 
 	"github.com/getfider/fider/app/pkg/log"
@@ -37,6 +37,7 @@ type BackgroundWorker struct {
 	queue      chan Task
 	len        int64
 	middleware MiddlewareFunc
+	sync.RWMutex
 }
 
 var maxQueueSize = 100
@@ -64,7 +65,9 @@ func (w *BackgroundWorker) Run(id string) {
 		}
 
 		w.middleware(task.Job)(c)
-		atomic.AddInt64(&w.len, -1)
+		w.Lock()
+		w.len = w.len - 1
+		w.Unlock()
 	}
 }
 
@@ -91,7 +94,9 @@ func (w *BackgroundWorker) Shutdown(ctx context.Context) error {
 
 //Enqueue a task on current worker
 func (w *BackgroundWorker) Enqueue(task Task) {
-	atomic.AddInt64(&w.len, 1)
+	w.Lock()
+	w.len = w.len + 1
+	w.Unlock()
 	w.queue <- task
 }
 
@@ -102,6 +107,8 @@ func (w *BackgroundWorker) Logger() log.Logger {
 
 //Length from current queue length
 func (w *BackgroundWorker) Length() int64 {
+	w.RLock()
+	defer w.RUnlock()
 	return w.len
 }
 

app/pkg/worker/worker_test.go
@@ -2,6 +2,7 @@ package worker_test
 
 import (
 	"context"
+	"sync"
 	"testing"
 	"time"
 
@@ -21,18 +22,24 @@ func TestBackgroundWorker(t *testing.T) {
 	RegisterT(t)
 
 	var finished bool
+	mu := &sync.RWMutex{}
 
 	w := worker.New()
 	w.Enqueue(worker.Task{
 		Name: "Do Something",
 		Job: func(c *worker.Context) error {
+			mu.Lock()
+			defer mu.Unlock()
 			finished = true
 			return nil
 		},
 	})
+
 	Expect(w.Length()).Equals(int64(1))
 	go w.Run("worker-1")
 	Expect(func() bool {
+		mu.RLock()
+		defer mu.RUnlock()
 		return finished
 	}).EventuallyEquals(true)
 }

commit deacd04f1f0ce4827cb79452bfab6f4b1dc4e7a0

fix: resolve vet messages caused by #427 (#428)

app/pkg/dbx/dbx.go
@@ -42,11 +42,11 @@ func NewWithLogger(logger log.Logger) *Database {
 type Database struct {
 	conn   *sql.DB
 	logger log.Logger
-	mapper RowMapper
+	mapper *RowMapper
 }
 
 // Begin returns a new SQL transaction
-func (db Database) Begin() (*Trx, error) {
+func (db *Database) Begin() (*Trx, error) {
 	tx, err := db.conn.Begin()
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to start new transaction")
@@ -55,14 +55,14 @@ func (db Database) Begin() (*Trx, error) {
 }
 
 // Close connection to database
-func (db Database) Close() error {
+func (db *Database) Close() error {
 	if db.conn != nil {
 		return db.conn.Close()
 	}
 	return nil
 }
 
-func (db Database) load(path string) {
+func (db *Database) load(path string) {
 	content, err := ioutil.ReadFile(env.Path(path))
 	if err != nil {
 		panic(errors.Wrap(err, "failed to read file %s", path))
@@ -75,14 +75,14 @@ func (db Database) load(path string) {
 }
 
 // Seed clean and insert new seed data for testing
-func (db Database) Seed() {
+func (db *Database) Seed() {
 	if env.IsTest() {
 		db.load("/app/pkg/dbx/setup.sql")
 	}
 }
 
 // Migrate the database to latest version
-func (db Database) Migrate() {
+func (db *Database) Migrate() {
 	db.logger.Infof("Running migrations...")
 	err := retry(10, func() error {
 		m, err := migrate.New(
@@ -106,13 +106,13 @@ func (db Database) Migrate() {
 type Trx struct {
 	tx     *sql.Tx
 	logger log.Logger
-	mapper RowMapper
+	mapper *RowMapper
 }
 
 var formatter = strings.NewReplacer("\t", "", "\n", " ")
 
 // Execute given SQL command
-func (trx Trx) Execute(command string, args ...interface{}) (int64, error) {
+func (trx *Trx) Execute(command string, args ...interface{}) (int64, error) {
 	if trx.logger.IsEnabled(log.DEBUG) {
 		command = formatter.Replace(command)
 		start := time.Now()
@@ -134,7 +134,7 @@ func (trx Trx) Execute(command string, args ...interface{}) (int64, error) {
 }
 
 // Scalar returns first row and first column
-func (trx Trx) Scalar(data interface{}, command string, args ...interface{}) error {
+func (trx *Trx) Scalar(data interface{}, command string, args ...interface{}) error {
 	if trx.logger.IsEnabled(log.DEBUG) {
 		command = formatter.Replace(command)
 		start := time.Now()
@@ -155,7 +155,7 @@ func (trx Trx) Scalar(data interface{}, command string, args ...interface{}) err
 }
 
 // Get first row and bind to given data
-func (trx Trx) Get(data interface{}, command string, args ...interface{}) error {
+func (trx *Trx) Get(data interface{}, command string, args ...interface{}) error {
 	if trx.logger.IsEnabled(log.DEBUG) {
 		command = formatter.Replace(command)
 		start := time.Now()
@@ -183,7 +183,7 @@ func (trx Trx) Get(data interface{}, command string, args ...interface{}) error 
 }
 
 // QueryIntArray executes given SQL command and return first column as int
-func (trx Trx) QueryIntArray(command string, args ...interface{}) ([]int, error) {
+func (trx *Trx) QueryIntArray(command string, args ...interface{}) ([]int, error) {
 	if trx.logger.IsEnabled(log.DEBUG) {
 		command = formatter.Replace(command)
 		start := time.Now()
@@ -211,7 +211,7 @@ func (trx Trx) QueryIntArray(command string, args ...interface{}) ([]int, error)
 }
 
 // Exists returns true if at least one record is found
-func (trx Trx) Exists(command string, args ...interface{}) (bool, error) {
+func (trx *Trx) Exists(command string, args ...interface{}) (bool, error) {
 	if trx.logger.IsEnabled(log.DEBUG) {
 		command = formatter.Replace(command)
 		start := time.Now()
@@ -230,7 +230,7 @@ func (trx Trx) Exists(command string, args ...interface{}) (bool, error) {
 }
 
 // Count returns number of rows
-func (trx Trx) Count(command string, args ...interface{}) (int, error) {
+func (trx *Trx) Count(command string, args ...interface{}) (int, error) {
 	if trx.logger.IsEnabled(log.DEBUG) {
 		command = formatter.Replace(command)
 		start := time.Now()
@@ -253,7 +253,7 @@ func (trx Trx) Count(command string, args ...interface{}) (int, error) {
 }
 
 //Select all matched rows bind to given data
-func (trx Trx) Select(data interface{}, command string, args ...interface{}) error {
+func (trx *Trx) Select(data interface{}, command string, args ...interface{}) error {
 	if trx.logger.IsEnabled(log.DEBUG) {
 		command = formatter.Replace(command)
 		start := time.Now()
@@ -288,7 +288,7 @@ func (trx Trx) Select(data interface{}, command string, args ...interface{}) err
 }
 
 // Commit current transaction
-func (trx Trx) Commit() error {
+func (trx *Trx) Commit() error {
 	err := trx.tx.Commit()
 	if err != nil {
 		return errors.Wrap(err, "failed to commit transaction")
@@ -297,7 +297,7 @@ func (trx Trx) Commit() error {
 }
 
 // Rollback current transaction
-func (trx Trx) Rollback() error {
+func (trx *Trx) Rollback() error {
 	err := trx.tx.Rollback()
 	if err != nil {
 		return errors.Wrap(err, "failed to rollback transaction")

app/pkg/dbx/mapping.go
@@ -15,8 +15,8 @@ type RowMapper struct {
 }
 
 //NewRowMapper creates a new instance of RowMapper
-func NewRowMapper() RowMapper {
-	return RowMapper{
+func NewRowMapper() *RowMapper {
+	return &RowMapper{
 		cache: make(map[reflect.Type]TypeMapper, 0),
 	}
 }