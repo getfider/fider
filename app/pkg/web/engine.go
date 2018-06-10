321efca67a20255163e928d2d80d75504e20e82f
goenning <me@goenning.net>
2018-06-10 08:01:41 +0100

    use curve preferences

--- app/pkg/web/engine.go
+++ app/pkg/web/engine.go
@@ -98,6 +98,10 @@ func (e *Engine) Start(address string) {
 			MinVersion:               tls.VersionTLS12,
 			MaxVersion:               tls.VersionTLS12,
 			PreferServerCipherSuites: true,
+			CurvePreferences: []tls.CurveID{
+				tls.X25519,
+				tls.CurveP256,
+			},
 			CipherSuites: []uint16{
 				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
