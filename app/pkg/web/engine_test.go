5aef99467b93b4836a0f36e7b64f97afb5434356
goenning <me@goenning.net>
2018-06-21 19:10:03 +0100

    reliable test

--- app/pkg/web/engine_test.go
+++ app/pkg/web/engine_test.go
@@ -3,7 +3,6 @@ package web_test
 import (
 	"net/http"
 	"testing"
-	"time"
 
 	"github.com/getfider/fider/app/models"
 
@@ -23,7 +22,11 @@ func TestEngine_StartRequestStop(t *testing.T) {
 	}
 
 	go w.Start(":8080")
-	time.Sleep(time.Second)
+	Expect(func() error {
+		_, err := http.Get("http://127.0.0.1:8080/hello")
+		return err
+	}).EventuallyEquals(nil)
+
 	resp, err := http.Get("http://127.0.0.1:8080/hello")
 	Expect(err).IsNil()
 	Expect(resp.StatusCode).Equals(http.StatusOK)
