2310273153c3ac3e9cdccbab4aea740c7b78d67b
goenning <me@goenning.net>
2018-06-24 11:25:56 +0100

    revert

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -9,40 +9,20 @@ start_fider () {
   docker rm -f $FIDER_CONTAINER $PG_CONTAINER || true
   docker run -d -e POSTGRES_USER=fider_e2e -e POSTGRES_PASSWORD=fider_e2e_pw --name $PG_CONTAINER postgres:9.6.8
   docker run --link $PG_CONTAINER:pg waisbrot/wait
-
-  if [[ $CI == '' ]]
-  then
-    docker run \
-      -d \
-      -p 3000:3000 \
-      -e HOST_MODE=$1 \
-      -e DATABASE_URL=postgres://fider_e2e:fider_e2e_pw@$PG_CONTAINER:5432/fider_e2e?sslmode=disable \
-      --env-file .env \
-      --link $PG_CONTAINER \
-      --name $FIDER_CONTAINER getfider/fider:e2e
-  else
-    docker run \
-      -d \
-      -p 3000:3000 \
-      -e HOST_MODE=$1 \
-      -e JWT_SECRET=SOME_RANDOM_TOKEN_JUST_FOR_TESTING \
-      -e AUTH_ENDPOINT=http://login.dev.fider.io:3000 \
-      -e EMAIL_NOREPLY=noreply@fider.io \
-      -e EMAIL_MAILGUN_API=$EMAIL_MAILGUN_API \
-      -e EMAIL_MAILGUN_DOMAIN=$EMAIL_MAILGUN_DOMAIN \
-      -e OAUTH_FACEBOOK_APPID=$OAUTH_FACEBOOK_APPID \
-      -e OAUTH_FACEBOOK_SECRET=$OAUTH_FACEBOOK_SECRET \
-      -e GO_ENV=development \
-      -e DATABASE_URL=postgres://fider_e2e:fider_e2e_pw@$PG_CONTAINER:5432/fider_e2e?sslmode=disable \
-      --link $PG_CONTAINER \
-      --name $FIDER_CONTAINER getfider/fider:e2e
-  fi
+  docker run \
+    -d \
+    -p 3000:3000 \
+    -e HOST_MODE=$1 \
+    -e DATABASE_URL=postgres://fider_e2e:fider_e2e_pw@$PG_CONTAINER:5432/fider_e2e?sslmode=disable \
+    --env-file .env \
+    --link $PG_CONTAINER \
+    --name $FIDER_CONTAINER getfider/fider:e2e
 }
 
 run_e2e () {
   start_fider $1
   echo "Running e2e tests ..."
-  npx jest ./e2e/$1.spec.ts
+  npx jest ./tests/$1.spec.ts
   if [[ $? == 1 ]] 
   then
     exit 1
