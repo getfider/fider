89a7894bd2187ba084c5ed7ab8cc19bce36b0cf8
Guilherme Oenning <me@goenning.net>
2018-07-16 07:27:11 +0100

    enhancement: add legal links to Footer (#464)

--- public/components/common/Footer.tsx
+++ public/components/common/Footer.tsx
@@ -1,12 +1,21 @@
 import "./Footer.scss";
 
 import * as React from "react";
+import { PrivacyPolicy, TermsOfService } from "@fider/components";
+import { Fider } from "@fider/services";
 
 export const Footer = () => {
   return (
     <div id="c-footer">
       <div className="container">
-        <a target="_blank" href="https://getfider.com/">
+        {Fider.settings.hasLegal && (
+          <div className="l-links">
+            <PrivacyPolicy />
+            &middot;
+            <TermsOfService />
+          </div>
+        )}
+        <a className="l-powered" target="_blank" href="https://getfider.com/">
           <img src="https://getfider.com/images/logo-100x100.png" alt="Fider" />
           <span>Powered by Fider</span>
         </a>
