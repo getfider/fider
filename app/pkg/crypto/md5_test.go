515519c94ba50350ec5ce12d8316353504313786
goenning <me@goenning.net>
2018-10-09 17:59:21 +0100

    refactor package

--- app/pkg/crypto/md5_test.go
+++ app/pkg/crypto/md5_test.go
@@ -1,16 +1,16 @@
-package md5_test
+package crypto_test
 
 import (
 	"testing"
 
 	. "github.com/getfider/fider/app/pkg/assert"
-	"github.com/getfider/fider/app/pkg/md5"
+	"github.com/getfider/fider/app/pkg/crypto"
 )
 
 func TestMD5Hash(t *testing.T) {
 	RegisterT(t)
 
-	hash := md5.Hash("Fider")
+	hash := crypto.MD5("Fider")
 
 	Expect(hash).Equals("3734538c8b650e4f354a55a436566bb6")
 }
