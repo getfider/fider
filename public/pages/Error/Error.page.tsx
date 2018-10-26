8e52991889730a201fb069b091d5ed4170d970df
Cooper Filby <cfilby2@uabmc.edu>
2018-10-25 21:18:19 -0500

    Fix linting error

--- public/pages/Error/Error.page.tsx
+++ public/pages/Error/Error.page.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import { Fider } from "@fider/services";
 import { TenantLogo } from "@fider/components";
 
-
 interface ErrorPageProps {
   title?: string;
   message?: string;
