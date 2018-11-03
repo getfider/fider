aed6c013f964aad86c1d0583937d883e31cea08d
goenning <me@goenning.net>
2018-11-03 18:52:14 +0000

    enhancement: create page for browser not supported

--- public/services/navigator.ts
+++ public/services/navigator.ts
@@ -4,11 +4,23 @@ const navigator = {
   url: () => {
     return window.location.href;
   },
+  isBrowserSupported: () => {
+    const ua = window.navigator.userAgent || window.navigator.appVersion;
+    const isIE = ua.indexOf("MSIE") >= 0;
+    if (isIE) {
+      // On IE, the only supported version is IE 11
+      return window.navigator.appVersion.indexOf("MSIE 11") >= 0;
+    }
+    return true;
+  },
   goHome: () => {
     window.location.href = "/";
   },
   goTo: (url: string) => {
-    window.location.href = url;
+    const isEqual = window.location.href === url || window.location.pathname === url;
+    if (!isEqual) {
+      window.location.href = url;
+    }
   },
   replaceState: (path: string): void => {
     if (history.replaceState) {
