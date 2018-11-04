6684abb89068ad39b2175a45c198e967d2a30cb0
Guilherme Oenning <me@goenning.net>
2018-11-04 12:13:48 +0000

    fix: older browsers cannot execute inline javascript with CSP (#621)

--- public/tsd.d.ts
+++ public/tsd.d.ts
@@ -4,11 +4,6 @@ declare global {
   interface Window {
     ga?: (cmd: string, evt: string, args?: any) => void;
     set: (key: string, value: any) => void;
-    __props: { [key: string]: any };
-    __contextID: string;
-    __user: CurrentUser | undefined;
-    __tenant: Tenant;
-    __settings: SystemSettings;
   }
 
   var __webpack_nonce__: string; // tslint:disable-line
