de8b38298c3585743a02b5ba868e1bd093ff8338
goenning <me@goenning.net>
2018-11-10 18:31:26 +0000

    API to Block/Unblock user

--- app/cmd/routes.go
+++ app/cmd/routes.go
@@ -146,6 +146,8 @@ func routes(r *web.Engine) *web.Engine {
 		ui.Post("/_api/admin/settings/privacy", handlers.UpdatePrivacy())
 		ui.Post("/_api/admin/oauth", handlers.SaveOAuthConfig())
 		ui.Post("/_api/admin/roles/:role/users", handlers.ChangeUserRole())
+		ui.Put("/_api/admin/users/:userID/block", handlers.BlockUser())
+		ui.Delete("/_api/admin/users/:userID/block", handlers.UnblockUser())
 	}
 
 	api := r.Group()
