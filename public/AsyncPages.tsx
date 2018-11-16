1ac084742fcc531fef22f1c52550ef666cedcc20
goenning <me@goenning.net>
2018-11-16 18:16:39 +0000

    enhancement: retry lazy import

--- public/AsyncPages.tsx
+++ public/AsyncPages.tsx
@@ -1,39 +1,67 @@
 import { lazy } from "react";
 
-export const AsyncHomePage = lazy(() => import("@fider/pages/Home/Home.page"));
+const retry = (fn: () => Promise<{ default: any }>, retriesLeft = 5, interval = 500): Promise<{ default: any }> => {
+  return new Promise((resolve, reject) => {
+    fn()
+      .then(resolve)
+      .catch(err => {
+        setTimeout(() => {
+          if (retriesLeft === 1) {
+            reject(err);
+            return;
+          }
+          retry(fn, interval, retriesLeft - 1).then(resolve, reject);
+        }, interval);
+      });
+  });
+};
 
-export const AsyncShowPostPage = lazy(() => import("@fider/pages/ShowPost/ShowPost.page"));
+export const AsyncHomePage = lazy(() => retry(() => import("@fider/pages/Home/Home.page")));
 
-export const AsyncManageMembersPage = lazy(() => import("@fider/pages/Administration/pages/ManageMembers.page"));
+export const AsyncShowPostPage = lazy(() => retry(() => import("@fider/pages/ShowPost/ShowPost.page")));
 
-export const AsyncManageTagsPage = lazy(() => import("@fider/pages/Administration/pages/ManageTags.page"));
+export const AsyncManageMembersPage = lazy(() =>
+  retry(() => import("@fider/pages/Administration/pages/ManageMembers.page"))
+);
 
-export const AsyncPrivacySettingsPage = lazy(() => import("@fider/pages/Administration/pages/PrivacySettings.page"));
+export const AsyncManageTagsPage = lazy(() => retry(() => import("@fider/pages/Administration/pages/ManageTags.page")));
 
-export const AsyncExportPage = lazy(() => import("@fider/pages/Administration/pages/Export.page"));
+export const AsyncPrivacySettingsPage = lazy(() =>
+  retry(() => import("@fider/pages/Administration/pages/PrivacySettings.page"))
+);
 
-export const AsyncInvitationsPage = lazy(() => import("@fider/pages/Administration/pages/Invitations.page"));
+export const AsyncExportPage = lazy(() => retry(() => import("@fider/pages/Administration/pages/Export.page")));
+
+export const AsyncInvitationsPage = lazy(() =>
+  retry(() => import("@fider/pages/Administration/pages/Invitations.page"))
+);
 
 export const AsyncManageAuthenticationPage = lazy(() =>
   import("@fider/pages/Administration/pages/ManageAuthentication.page")
 );
 
-export const AsyncAdvancedSettingsPage = lazy(() => import("@fider/pages/Administration/pages/AdvancedSettings.page"));
+export const AsyncAdvancedSettingsPage = lazy(() =>
+  retry(() => import("@fider/pages/Administration/pages/AdvancedSettings.page"))
+);
 
-export const AsyncGeneralSettingsPage = lazy(() => import("@fider/pages/Administration/pages/GeneralSettings.page"));
+export const AsyncGeneralSettingsPage = lazy(() =>
+  retry(() => import("@fider/pages/Administration/pages/GeneralSettings.page"))
+);
 
-export const AsyncSignInPage = lazy(() => import("@fider/pages/SignIn/SignIn.page"));
+export const AsyncSignInPage = lazy(() => retry(() => import("@fider/pages/SignIn/SignIn.page")));
 
-export const AsyncSignUpPage = lazy(() => import("@fider/pages/SignUp/SignUp.page"));
+export const AsyncSignUpPage = lazy(() => retry(() => import("@fider/pages/SignUp/SignUp.page")));
 
 export const AsyncCompleteSignInProfilePage = lazy(() =>
   import("@fider/pages/CompleteSignInProfile/CompleteSignInProfile.page")
 );
 
-export const AsyncMyNotificationsPage = lazy(() => import("@fider/pages/MyNotifications/MyNotifications.page"));
+export const AsyncMyNotificationsPage = lazy(() =>
+  retry(() => import("@fider/pages/MyNotifications/MyNotifications.page"))
+);
 
-export const AsyncMySettingsPage = lazy(() => import("@fider/pages/MySettings/MySettings.page"));
+export const AsyncMySettingsPage = lazy(() => retry(() => import("@fider/pages/MySettings/MySettings.page")));
 
-export const AsyncOAuthEchoPage = lazy(() => import("@fider/pages/OAuthEcho/OAuthEcho.page"));
+export const AsyncOAuthEchoPage = lazy(() => retry(() => import("@fider/pages/OAuthEcho/OAuthEcho.page")));
 
-export const AsyncUIToolkitPage = lazy(() => import("@fider/pages/UI/UIToolkit.page"));
+export const AsyncUIToolkitPage = lazy(() => retry(() => import("@fider/pages/UI/UIToolkit.page")));
