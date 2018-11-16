860b9e444ddbd84f3b16c659629879cd192185b0
Oxynux <oxynux@gmail.com>
2018-11-16 12:39:26 +0100

    update agnosticAuth to avoid duplicate code

--- app/pkg/email/smtp/auth.go
+++ app/pkg/email/smtp/auth.go
@@ -2,41 +2,40 @@ package smtp
 
 import (
 	gosmtp "net/smtp"
-
-	"github.com/getfider/fider/app/pkg/errors"
 )
 
 type agnosticAuth struct {
 	identity, username, password, host string
-	auth                               string
+	auth                               gosmtp.Auth
+}
+
+func (a *agnosticAuth) FindAuth(server *gosmtp.ServerInfo) gosmtp.Auth {
+	for _, auth := range server.Auth {
+		switch auth {
+		case "LOGIN":
+			return LoginAuth(a.username, a.password)
+		case "PLAIN":
+			return gosmtp.PlainAuth(a.identity, a.username, a.password, a.host)
+		case "CRAM-MD5":
+			return gosmtp.CRAMMD5Auth(a.username, a.password)
+		default:
+			continue
+		}
+	}
+	return nil
 }
 
 // AgnosticAuth returns an Auth that match the correct authentication
 // thanks to gosmtp.ServerInfo
 func AgnosticAuth(identity, username, password, host string) gosmtp.Auth {
-	return &agnosticAuth{identity, username, password, host, ""}
+	return &agnosticAuth{identity, username, password, host, nil}
 }
 
 func (a *agnosticAuth) Start(server *gosmtp.ServerInfo) (string, []byte, error) {
-	for _, auth := range server.Auth {
-		if a.auth = auth; a.auth == "LOGIN" {
-			return LoginAuth(a.username, a.password).Start(server)
-		} else if a.auth == "PLAIN" {
-			return gosmtp.PlainAuth(a.identity, a.username, a.password, a.host).Start(server)
-		} else if a.auth == "CRAM-MD5" {
-			return gosmtp.CRAMMD5Auth(a.username, a.password).Start(server)
-		}
-	}
-	return "", nil, errors.New("unknown auth method")
+	a.auth = a.FindAuth(server)
+	return a.auth.Start(server)
 }
 
 func (a *agnosticAuth) Next(fromServer []byte, more bool) ([]byte, error) {
-	if a.auth == "LOGIN" {
-		return LoginAuth(a.username, a.password).Next(fromServer, more)
-	} else if a.auth == "PLAIN" {
-		return gosmtp.PlainAuth(a.identity, a.username, a.password, a.host).Next(fromServer, more)
-	} else if a.auth == "CRAM-MD5" {
-		return gosmtp.CRAMMD5Auth(a.username, a.password).Next(fromServer, more)
-	}
-	return nil, errors.New("unknown auth method")
+	return a.auth.Next(fromServer, more)
 }
