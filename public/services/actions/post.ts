e7ace744f6632848db2f7b0e923c16fe0c8f07f0
goenning <me@goenning.net>
2018-11-27 07:32:57 +0000

    wip for UI to show all voters

--- public/services/actions/post.ts
+++ public/services/actions/post.ts
@@ -1,5 +1,5 @@
 import { http, Result, querystring } from "@fider/services";
-import { Post } from "@fider/models";
+import { Post, User } from "@fider/models";
 
 export const getAllPosts = async (): Promise<Result<Post[]>> => {
   return await http.get<Post[]>("/api/v1/posts");
@@ -45,6 +45,10 @@ export const subscribe = async (postNumber: number): Promise<Result> => {
 
 export const unsubscribe = async (postNumber: number): Promise<Result> => {
   return http.delete(`/api/v1/posts/${postNumber}/subscription`).then(http.event("post", "unsubscribe"));
+};
+
+export const listVoters = async (postNumber: number): Promise<Result<User[]>> => {
+  return http.get<User[]>(`/api/v1/posts/${postNumber}/voters`);
 };
 
 export const createComment = async (postNumber: number, content: string): Promise<Result> => {
