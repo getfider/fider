664c239cf934b400aed6672393d23c79f5225dd2
Guilherme Oenning <me@goenning.net>
2018-11-30 07:38:47 +0000

    fix: dbx was setting to null when there were no values in array (#659)

--- public/pages/Error/Error.page.tsx
+++ public/pages/Error/Error.page.tsx
@@ -14,31 +14,34 @@ interface ErrorPageProps {
 }
 
 const defaultProps: Partial<ErrorPageProps> = {
-  title: "Whoops!",
-  message: "An unexpected rendering error has occurred.",
+  title: "Shoot! Well, this is unexpected…",
+  message: "An error has occurred and we're working to fix the problem!",
   showError: () => !Fider.isProduction()
 };
 
 export const ErrorPage: React.SFC<ErrorPageProps> = ({
-  title = "Whoops!",
-  message = "An unexpected rendering error has occurred.",
+  title = "Shoot! Well, this is unexpected…",
+  message = "An error has occurred and we're working to fix the problem!",
   error,
   errorInfo,
   showError
 }: ErrorPageProps) => {
   return (
     <div id="p-error" className="container failure-page">
-      <TenantLogo size={100} />
-      <div className="content">
-        <h2>{title}</h2>
-        <p>{message}</p>
-        {showError && showError() && (
-          <pre className="error">
-            {error.toString()}
-            {errorInfo.componentStack}
-          </pre>
-        )}
-      </div>
+      <TenantLogo size={100} useFiderIfEmpty={true} />
+      <h1>{title}</h1>
+      <p>{message}</p>
+      {Fider.settings && (
+        <span>
+          Take me back to <a href={Fider.settings.baseURL}>{Fider.settings.baseURL}</a> home page.
+        </span>
+      )}
+      {showError && showError() && (
+        <pre className="error">
+          {error.toString()}
+          {errorInfo.componentStack}
+        </pre>
+      )}
     </div>
   );
 };
