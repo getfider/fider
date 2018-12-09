2b74f27afb80e490abd97f953991f65cea7de270
goenning <me@goenning.net>
2018-12-09 17:03:32 +0000

    refactor

--- app/middlewares/maintenance.go
+++ app/middlewares/maintenance.go
@@ -15,11 +15,12 @@ func Maintenance() web.MiddlewareFunc {
 				return next(c)
 			}
 
+			c.Response.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+			c.Response.Header().Set("Retry-After", "3600")
+
 			message := env.GetEnvOrDefault("MAINTENANCE_MESSAGE", "Sorry, we're down for scheduled maintenance right now.")
 			until := env.GetEnvOrDefault("MAINTENANCE_UNTIL", "")
 
-			c.Response.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
-			c.Response.Header().Set("Retry-After", "3600")
 			return c.Render(http.StatusServiceUnavailable, "maintenance.html", web.Props{
 				Title:       "UNDER MAINTENANCE",
 				Description: message,
