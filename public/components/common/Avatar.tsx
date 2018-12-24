6870cca15a2224bc405cb553746681bec39feea8
goenning <me@goenning.net>
2018-12-24 07:26:24 +0000

    refactor Avatar component

--- public/components/common/Avatar.tsx
+++ public/components/common/Avatar.tsx
@@ -1,31 +1,25 @@
 import "./Avatar.scss";
 
 import React from "react";
-import { classSet, Fider } from "@fider/services";
+import { classSet } from "@fider/services";
 import { isCollaborator, UserRole } from "@fider/models";
 
 interface AvatarProps {
-  user?: {
-    id?: number;
-    name: string;
+  user: {
     role?: UserRole;
-    avatarURL?: string;
+    avatarURL: string;
   };
   size?: "small" | "normal" | "large";
 }
 
 export const Avatar = (props: AvatarProps) => {
   const size = props.size || "normal";
-  const id = props.user ? props.user.id : 0;
-  const name = props.user ? props.user.name : "";
-  const url = `${Fider.settings.tenantAssetsURL}/avatars/letter/${id}/${encodeURIComponent(name || "?")}`;
-  const avatarURL = props.user ? props.user.avatarURL || url : url;
 
   const className = classSet({
     "c-avatar": true,
     [`m-${size}`]: true,
-    "m-staff": props.user && props.user.role && isCollaborator(props.user.role)
+    "m-staff": props.user.role && isCollaborator(props.user.role)
   });
 
-  return <img className={className} title={name} src={`${avatarURL}?size=50`} />;
+  return <img className={className} title={name} src={`${props.user.avatarURL}?size=50`} />;
 };
