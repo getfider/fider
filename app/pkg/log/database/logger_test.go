091abba495ad6be09171726b9720cafbcdfc9263
Guilherme Oenning <me@goenning.net>
2019-02-25 21:04:33 +0000

    fix: write logs on a separate goroutine (#725)

--- app/pkg/log/database/logger_test.go
+++ app/pkg/log/database/logger_test.go
@@ -19,8 +19,9 @@ func TestLog(t *testing.T) {
 	RegisterT(t)
 	db := dbx.New()
 	defer db.Close()
+	db.Connection().Exec("DELETE FROM logs WHERE properties->>'ContextID' = 'MyContextID'")
+
 	trx, _ := db.Begin()
-	trx.Execute("DELETE FROM logs WHERE properties->>'ContextID' = 'MyContextID'")
 	defer trx.Commit()
 
 	logger := database.NewLogger("TEST", db)
@@ -30,6 +31,12 @@ func TestLog(t *testing.T) {
 
 	logger.Infof("2 + 2 is @{Result}", log.Props{"Result": 4})
 	logger.Debugf("2 + 2 is @{Result}", log.Props{"Result": 4})
+
+	Expect(func() int {
+		count, err := trx.Count("SELECT * FROM logs WHERE properties->>'ContextID' = 'MyContextID'")
+		Expect(err).IsNil()
+		return count
+	}).EventuallyEquals(1)
 
 	var entries []*logEntry
 	err := trx.Select(&entries, "SELECT tag, level, text FROM logs WHERE properties->>'ContextID' = 'MyContextID'")
