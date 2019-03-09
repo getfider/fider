1dd69633956adf1184a2cc2b1b3ff50b1139470c
goenning <me@goenning.net>
2019-03-09 14:24:46 +0000

    refactor: migrate Loader to React Hooks

---
+++ public/hooks/use-timeout.ts
@@ -0,0 +1,23 @@
+import { useRef, useEffect } from "react";
+
+type CallbackFunction = () => void;
+
+export function useTimeout(callback: CallbackFunction, delay: number) {
+  const savedCallback = useRef<CallbackFunction>();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  useEffect(() => {
+    function tick() {
+      if (savedCallback.current) {
+        savedCallback.current();
+      }
+    }
+    const timer = window.setTimeout(tick, delay);
+    return function cleanup() {
+      window.clearTimeout(timer);
+    };
+  }, [delay]);
+}
