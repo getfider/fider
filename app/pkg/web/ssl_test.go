7a19f04e4e8d1f361ef9fb769332d513e02fddda
goenning <me@goenning.net>
2019-04-03 08:09:37 +0100

    refactor test files

--- app/pkg/web/ssl_test.go
+++ app/pkg/web/ssl_test.go
@@ -4,9 +4,7 @@ import (
 	"crypto/tls"
 	"testing"
 
-	"github.com/getfider/fider/app/pkg/bus"
 	"github.com/getfider/fider/app/pkg/env"
-	"github.com/getfider/fider/app/services/blob/fs"
 
 	. "github.com/getfider/fider/app/pkg/assert"
 )
@@ -49,23 +47,5 @@ func Test_GetCertificate(t *testing.T) {
 			Expect(cert).IsNil()
 			Expect(err.Error()).ContainsSubstring(`ssl: invalid server name "` + testCase.serverName + `"`)
 		}
-	}
-}
-
-func Test_UseAutoCert(t *testing.T) {
-	RegisterT(t)
-	bus.Init(fs.Service{})
-
-	manager, err := NewCertificateManager("", "")
-	Expect(err).IsNil()
-
-	invalidServerNames := []string{"ideas.app.com", "feedback.mysite.com"}
-
-	for _, serverName := range invalidServerNames {
-		cert, err := manager.GetCertificate(&tls.ClientHelloInfo{
-			ServerName: serverName,
-		})
-		Expect(err.Error()).ContainsSubstring(`acme/autocert: unable to authorize "` + serverName + `"; challenge "tls-alpn-01" failed with error: acme: authorization error`)
-		Expect(cert).IsNil()
 	}
 }
