f958d1a14738161df139177df3857d9526aac58a
goenning <me@goenning.net>
2019-05-11 21:40:20 +0100

    add blobs to backup file

--- app/pkg/backup/backup.go
+++ app/pkg/backup/backup.go
@@ -6,6 +6,9 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/getfider/fider/app/models/query"
+	"github.com/getfider/fider/app/pkg/bus"
+
 	"github.com/getfider/fider/app/pkg/errors"
 )
 
@@ -36,12 +39,43 @@ func Create(ctx context.Context) (*bytes.Buffer, error) {
 		}
 	}
 
+	listBlobs := &query.ListBlobs{}
+	if err := bus.Dispatch(ctx, listBlobs); err != nil {
+		return nil, err
+	}
+
+	for _, bkey := range listBlobs.Result {
+		err := addBlobToZipFile(ctx, zipWriter, bkey)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	err := zipWriter.Close()
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to close zip file")
 	}
 
 	return buffer, nil
+}
+
+func addBlobToZipFile(ctx context.Context, zipWriter *zip.Writer, bkey string) error {
+	getBlob := &query.GetBlobByKey{Key: bkey}
+	if err := bus.Dispatch(ctx, getBlob); err != nil {
+		return err
+	}
+
+	fileName := fmt.Sprintf("blobs/%s", bkey)
+	fileWriter, err := zipWriter.Create(fileName)
+	if err != nil {
+		return errors.Wrap(err, "failed to create %s in zip file", fileName)
+	}
+	_, err = fileWriter.Write(getBlob.Result.Content)
+	if err != nil {
+		return errors.Wrap(err, "failed to write %s to zip file", fileName)
+	}
+
+	return nil
 }
 
 func addTableDataToZipFile(ctx context.Context, zipWriter *zip.Writer, tableName string) error {
