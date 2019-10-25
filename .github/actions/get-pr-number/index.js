3908f75574993804162f72212a35a1849b94c253
goenning <me@goenning.net>
2019-10-25 10:28:38 +0100

    use action

--- .github/actions/get-pr-number/index.js
+++
@@ -1,8 +0,0 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-
-try {
-  core.debug(github.context.payload.pull_request);
-} catch (error) {
-  core.setFailed(error.message);
-}
