62709be8ee932cbcd2cc386a04e05ee857c53344
Guilherme Oenning <me@goenning.net>
2019-11-03 20:55:39 +0000

    refactor: simplify gzip middleware (#803)

--- app/middlewares/compress.go
+++ app/middlewares/compress.go
@@ -1,76 +1,86 @@
 package middlewares
 
 import (
-	"bufio"
-	"bytes"
 	"compress/gzip"
-	"io/ioutil"
-	"net"
+	"io"
 	"net/http"
 	"strings"
-	"sync"
 
 	"github.com/getfider/fider/app/pkg/web"
 )
 
-var (
-	minSize = 1024
-)
-
 type gzipResponseWriter struct {
-	code     int
-	response http.ResponseWriter
-	writer   *gzip.Writer
-	buffer   *bytes.Buffer
+	io.Writer
+	http.ResponseWriter
+	http.Hijacker
+	http.Flusher
+	http.CloseNotifier
 }
 
 // Compress returns a middleware which compresses HTTP response using gzip compression
 func Compress() web.MiddlewareFunc {
 	return func(next web.HandlerFunc) web.HandlerFunc {
-		var pool sync.Pool
-		pool.New = func() interface{} {
-			return gzip.NewWriter(ioutil.Discard)
-		}
 		return func(c *web.Context) error {
 			res := c.Response
 			if strings.Contains(c.Request.GetHeader("Accept-Encoding"), "gzip") {
-				writer := pool.Get().(*gzip.Writer)
-				defer pool.Put(writer)
-				gzipResponse := &gzipResponseWriter{response: res, writer: writer, buffer: &bytes.Buffer{}}
-				c.Response = gzipResponse
-				defer gzipResponse.Flush()
+
+				gw, _ := gzip.NewWriterLevel(res, gzip.DefaultCompression)
+				defer gw.Close()
+
+				res.Header().Set("Content-Encoding", "gzip")
+				res.Header().Del("Accept-Encoding")
+				res.Header().Add("Vary", "Accept-Encoding")
+
+				h, hok := res.(http.Hijacker)
+				if !hok { /* w is not Hijacker... oh well... */
+					h = nil
+				}
+
+				f, fok := res.(http.Flusher)
+				if !fok {
+					f = nil
+				}
+
+				c.Response = &gzipResponseWriter{
+					Writer:         gw,
+					ResponseWriter: res,
+					Hijacker:       h,
+					Flusher:        f,
+				}
 			}
 			return next(c)
 		}
 	}
 }
 func (w *gzipResponseWriter) Header() http.Header {
-	return w.response.Header()
+	return w.ResponseWriter.Header()
 }
 
 func (w *gzipResponseWriter) WriteHeader(code int) {
-	w.code = code
+	w.ResponseWriter.Header().Del("Content-Length")
+	w.ResponseWriter.WriteHeader(code)
+
 }
 
 func (w *gzipResponseWriter) Write(b []byte) (int, error) {
-	return w.buffer.Write(b)
+	header := w.ResponseWriter.Header()
+	if header.Get("Content-Type") == "" {
+		header.Set("Content-Type", http.DetectContentType(b))
+	}
+	header.Del("Content-Length")
+
+	return w.Writer.Write(b)
+}
+
+type flusher interface {
+	Flush() error
 }
 
 func (w *gzipResponseWriter) Flush() {
-	w.Header().Add("Vary", "Accept-Encoding")
-	if w.buffer.Len() >= minSize {
-		w.Header().Del("Content-Length")
-		w.Header().Set("Content-Encoding", "gzip")
-		w.response.WriteHeader(w.code)
-		w.writer.Reset(w.response)
-		_, _ = w.buffer.WriteTo(w.writer)
-		w.writer.Close()
-	} else if w.code > 0 {
-		w.response.WriteHeader(w.code)
-		_, _ = w.buffer.WriteTo(w.response)
+	if f, ok := w.Writer.(flusher); ok {
+		f.Flush()
 	}
-}
-
-func (w *gzipResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
-	return w.response.(http.Hijacker).Hijack()
+	if w.Flusher != nil {
+		w.Flusher.Flush()
+	}
 }
