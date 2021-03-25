578cdbb7372f07d2780ac3473697b61129787314
goenning <me@goenning.net>
2021-03-25 08:21:40 +0000

    remove from footer

--- public/components/common/Footer.tsx
+++ public/components/common/Footer.tsx
@@ -1,22 +1,11 @@
 import "./Footer.scss"
 
 import React from "react"
-import { PrivacyPolicy, TermsOfService } from "@fider/components"
-import { useFider } from "@fider/hooks"
 
 export const Footer = () => {
-  const fider = useFider()
-
   return (
     <div id="c-footer">
       <div className="container">
-        {fider.settings.hasLegal && (
-          <div className="l-links">
-            <PrivacyPolicy />
-            &middot;
-            <TermsOfService />
-          </div>
-        )}
         <a rel="noopener" className="l-powered" target="_blank" href="https://getfider.com/">
           <img src="https://getfider.com/images/logo-100x100.png" alt="Fider" />
           <span>Powered by Fider</span>
