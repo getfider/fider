c69c3dc8b5ef69b0a8f62a63738dd94cad5e3c0e
goenning <me@goenning.net>
2021-04-07 20:44:00 +0100

    date format

--- public/components/common/Moment.tsx
+++ public/components/common/Moment.tsx
@@ -12,9 +12,12 @@ export const Moment = (props: MomentText) => {
   }
 
   const format = props.format || "relative"
+
   const now = new Date()
   const date = props.date instanceof Date ? props.date : new Date(props.date)
-  const display = format === "relative" ? timeSince(now, date) : formatDate(props.date, format)
+  const diff = (now.getTime() - date.getTime()) / (60 * 60 * 24 * 1000)
+  const display =
+    diff >= 365 && format === "relative" ? formatDate(props.date, "short") : format === "relative" ? timeSince(now, date) : formatDate(props.date, format)
 
   return (
     <span className="date" title={formatDate(props.date, "full")}>
