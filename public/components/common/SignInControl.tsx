dc208dacdfe24cf786ee0ec6d366271ca2b4abf0
Karsten Küpper <karsten.kuepper@bcc.no>
2021-04-22 20:23:31 +0200

    fix

--- public/components/common/SignInControl.tsx
+++ public/components/common/SignInControl.tsx
@@ -30,7 +30,6 @@ export const SignInControl: React.FunctionComponent<SignInControlProps> = (props
   }
 
   const providersLen = fider.settings.oauth.length
-  const loginUrl = fider.settings.oauth[0].url+'?redirect='+props.redirectTo;
 
   if (!isCookieEnabled()) {
     return (
@@ -42,6 +41,7 @@ export const SignInControl: React.FunctionComponent<SignInControlProps> = (props
   }
 
   if (providersLen > 0) {
+    const loginUrl = fider.settings.oauth[0].url+'?redirect='+props.redirectTo;
     location.replace(loginUrl);
     return (
       <div>redirecting you to login automatically. <a href={loginUrl}>click here</a> if it doesn't work</div>
