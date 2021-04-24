dd4a8ad25eadd2beffe0ad493244d8550da50860
goenning <me@goenning.net>
2021-04-24 08:06:31 +0100

    add wrapper class

--- public/components/UserMenu.tsx
+++ public/components/UserMenu.tsx
@@ -6,19 +6,21 @@ export const UserMenu = () => {
   const fider = useFider()
 
   return (
-    <Dropdown position="left" renderHandle={<Avatar user={fider.session.user} />}>
-      <div className="p-2 text-medium uppercase">{fider.session.user.name}</div>
-      <Dropdown.ListItem href="/settings">My Settings</Dropdown.ListItem>
-      <Dropdown.Divider />
+    <div className="c-menu-user">
+      <Dropdown position="left" renderHandle={<Avatar user={fider.session.user} />}>
+        <div className="p-2 text-medium uppercase">{fider.session.user.name}</div>
+        <Dropdown.ListItem href="/settings">My Settings</Dropdown.ListItem>
+        <Dropdown.Divider />
 
-      {fider.session.user.isCollaborator && (
-        <>
-          <div className="p-2 text-medium uppercase">Administration</div>
-          <Dropdown.ListItem href="/admin">Site Settings</Dropdown.ListItem>
-          <Dropdown.Divider />
-        </>
-      )}
-      <Dropdown.ListItem href="/signout?redirect=/">Sign out</Dropdown.ListItem>
-    </Dropdown>
+        {fider.session.user.isCollaborator && (
+          <>
+            <div className="p-2 text-medium uppercase">Administration</div>
+            <Dropdown.ListItem href="/admin">Site Settings</Dropdown.ListItem>
+            <Dropdown.Divider />
+          </>
+        )}
+        <Dropdown.ListItem href="/signout?redirect=/">Sign out</Dropdown.ListItem>
+      </Dropdown>
+    </div>
   )
 }
