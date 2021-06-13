1d259b721c54215a30e1a22282ea620605d71b7d
goenning <me@goenning.net>
2021-06-13 08:34:03 +0100

    refactor

--- public/services/i18n.ts
+++ public/services/i18n.ts
@@ -12,20 +12,14 @@ export function activateI18NSync(locale: string, messages: any): I18n {
 }
 
 export async function activateI18N(locale: string): Promise<I18n> {
-  const i18n = setupI18n({ missing: (_, key) => `⚠️ Missing Translation: ${key}` })
-  locale = locale || "en"
   try {
     const content = await import(
       /* webpackChunkName: "locale-[request]" */
       `@locale/client/${locale}.po`
     )
-
-    i18n.loadLocaleData("en", { plurals: en })
-    i18n.loadLocaleData("pt-BR", { plurals: pt })
-    i18n.load(locale, content.messages)
-    i18n.activate(locale)
+    return activateI18NSync(locale, content.messages)
   } catch (err) {
     console.error(err)
+    return activateI18NSync(locale, {})
   }
-  return i18n
 }
