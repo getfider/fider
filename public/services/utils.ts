a5e6f4b9a5fea43c05889077d2e2775231f449e2
goenning <me@goenning.net>
2021-07-06 20:50:10 +0100

    feature: translate date display

--- public/services/utils.ts
+++ public/services/utils.ts
@@ -17,79 +17,42 @@ export const classSet = (input?: any): string => {
   return ""
 }
 
-const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-
-export const currencySymbol = (currencyCode: string): string => {
-  currencyCode = currencyCode ? currencyCode.toLowerCase() : ""
-  switch (currencyCode) {
-    case "eur":
-      return "€"
-    case "usd":
-      return "$"
-  }
-  throw new Error(`Invalid currency code '${currencyCode}'`)
-}
-
-const twoDigits = (value: number): string => {
-  return value <= 9 ? `0${value}` : value.toString()
-}
-
 type DateFormat = "full" | "short"
+const shortOpts: Intl.DateTimeFormatOptions = { month: "short", year: "2-digit" }
+const fullOpts: Intl.DateTimeFormatOptions = { day: "2-digit", month: "long", year: "numeric", hour: "numeric", minute: "numeric" }
 
-export const formatDate = (input: Date | string, format: DateFormat = "full"): string => {
+export const formatDate = (locale: string, input: Date | string, format: DateFormat = "full"): string => {
   const date = input instanceof Date ? input : new Date(input)
 
-  const monthIndex = date.getMonth()
-  const year = date.getFullYear()
-
-  if (format === "short") {
-    return `${monthNames[monthIndex].substring(0, 3)} '${year.toString().substring(2)}`
+  try {
+    const opts = format === "short" ? shortOpts : fullOpts
+    return new Intl.DateTimeFormat(locale, opts).format(date)
+  } catch {
+    return date.toLocaleString(locale)
   }
-
-  const day = date.getDate()
-  const hours = twoDigits(date.getHours())
-  const minutes = twoDigits(date.getMinutes())
-  return `${monthNames[monthIndex]} ${day}, ${year} · ${hours}:${minutes}`
 }
 
-const templates: { [key: string]: string } = {
-  seconds: "less than a minute",
-  minute: "about a minute",
-  minutes: "%d minutes",
-  hour: "about an hour",
-  hours: "about %d hours",
-  day: "a day",
-  days: "%d days",
-  month: "about a month",
-  months: "%d months",
-  year: "about a year",
-  years: "%d years",
-}
+export const timeSince = (locale: string, now: Date, date: Date): string => {
+  try {
+    const seconds = (now.getTime() - date.getTime()) / 1000
+    const minutes = Math.round(seconds / 60)
+    const hours = Math.round(minutes / 60)
+    const days = Math.round(hours / 24)
+    const months = Math.round(days / 30)
+    const years = Math.round(days / 365)
 
-const template = (t: string, n: number): string => {
-  return templates[t] && templates[t].replace(/%d/i, Math.abs(Math.round(n)).toString())
-}
-
-export const timeSince = (now: Date, date: Date): string => {
-  const seconds = (now.getTime() - date.getTime()) / 1000
-  const minutes = seconds / 60
-  const hours = minutes / 60
-  const days = hours / 24
-  const years = days / 365
-
-  return (
-    ((seconds < 45 && template("seconds", seconds)) ||
-      (seconds < 90 && template("minute", 1)) ||
-      (minutes < 45 && template("minutes", minutes)) ||
-      (minutes < 90 && template("hour", 1)) ||
-      (hours < 24 && template("hours", hours)) ||
-      (hours < 42 && template("day", 1)) ||
-      (days < 30 && template("days", days)) ||
-      (days < 45 && template("month", 1)) ||
-      (days < 365 && template("months", days / 30)) ||
-      (years < 1.5 && template("year", 1)) ||
-      template("years", years)) + " ago"
-  )
+    const rtf = new Intl.RelativeTimeFormat(locale, { numeric: "auto" })
+    return (
+      (seconds < 60 && rtf.format(-1 * seconds, "seconds")) ||
+      (minutes < 60 && rtf.format(-1 * minutes, "minutes")) ||
+      (hours < 24 && rtf.format(-1 * hours, "hours")) ||
+      (days < 30 && rtf.format(-1 * days, "days")) ||
+      (days < 365 && rtf.format(-1 * months, "months")) ||
+      rtf.format(-1 * years, "years")
+    )
+  } catch {
+    return formatDate(locale, date, "short")
+  }
 }
 
 export const fileToBase64 = async (file: File): Promise<string> => {
