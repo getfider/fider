9478880d5ab61ba98a748d09121167fefd4f8a18
goenning <me@goenning.net>
2021-07-17 07:00:44 +0100

    log condition

--- app/pkg/web/ssl.go
+++ app/pkg/web/ssl.go
@@ -39,16 +39,17 @@ func getDefaultTLSConfig(autoSSL bool) *tls.Config {
 	}
 }
 
+var errInvalidHostName = errors.New("autossl: invalid hostname")
 func isValidHostName(ctx context.Context, host string) error {
 	if host == "" {
-		return errors.New("host cannot be empty.")
+		return errors.Wrap(errInvalidHostName, "host cannot be empty.")
 	}
 
 	if env.IsSingleHostMode() {
 		if env.Config.HostDomain == host {
 			return nil
 		}
-		return errors.New("server name mismatch")
+		return errors.Wrap(errInvalidHostName, "server name mismatch")
 	}
 
 	trx, err := dbx.BeginTx(ctx)
@@ -64,7 +65,7 @@ func isValidHostName(ctx context.Context, host string) error {
 	}
 
 	if isAvailable.Result {
-		return errors.New("no tenants found with cname %s", host)
+		return errors.Wrap(errInvalidHostName, "no tenants found with cname %s", host)
 	}
 	return nil
 }
@@ -85,7 +86,6 @@ func NewCertificateManager(ctx context.Context, certFile, keyFile string) (*Cert
 			Prompt:     autocert.AcceptTOS,
 			Cache:      NewAutoCertCache(),
 			Client:     acmeClient(),
-			HostPolicy: isValidHostName,
 		},
 	}
 
@@ -134,8 +134,8 @@ func (m *CertificateManager) GetCertificate(hello *tls.ClientHelloInfo) (*tls.Ce
 	}
 
 	cert, err := m.autossl.GetCertificate(hello)
-	if err != nil {
-		log.Error(m.ctx, errors.Wrap(err, "unable to get certificate from acme client"))
+	if err != nil && errors.Cause(err) != errInvalidHostName {
+		log.Error(m.ctx, err)
 	}
 
 	return cert, err
