5d090b51be5e55c95804477794a5763789fd3ac9
Anass Daoudi <anass1995daoudi@gmail.com>
2021-10-24 20:47:24 +0100

    Migrate Select component to functional style

--- public/components/common/form/Select.tsx
+++ public/components/common/form/Select.tsx
@@ -17,78 +17,40 @@ interface SelectProps {
   onChange?: (option?: SelectOption) => void;
 }
 
-interface SelectState {
-  selected?: SelectOption;
-}
+export const Select: React.FC<SelectProps> = (props) => {
+  const onChange = (e: React.FormEvent<HTMLSelectElement>) => {
+    const selectedOption = e.currentTarget.value
+      ? props.options.find(({ value }) => value === e.currentTarget.value)
+      : undefined;
 
-export class Select extends React.Component<SelectProps, SelectState> {
-  constructor(props: SelectProps) {
-    super(props);
-    this.state = {
-      selected: this.getOption(props.defaultValue)
-    };
-  }
-
-  private getOption(value?: string): SelectOption | undefined {
-    if (value && this.props.options) {
-      const filtered = this.props.options.filter(x => x.value === value);
-      if (filtered && filtered.length > 0) {
-        return filtered[0];
-      }
-    }
-  }
-
-  private onChange = (e: React.FormEvent<HTMLSelectElement>) => {
-    let selected: SelectOption | undefined;
-    if (e.currentTarget.value) {
-      const options = this.props.options.filter(o => o.value === e.currentTarget.value);
-      if (options && options.length > 0) {
-        selected = options[0];
-      }
-    }
-
-    this.setState({ selected }, () => {
-      if (this.props.onChange) {
-        this.props.onChange(this.state.selected);
-      }
-    });
+    props.onChange?.(selectedOption);
   };
 
-  public render() {
-    const options = this.props.options.map(option => {
-      return (
-        <option key={option.value} value={option.value}>
-          {option.label}
-        </option>
-      );
-    });
+  const options = props.options.map((option) => (
+    <option key={option.value} value={option.value}>
+      {option.label}
+    </option>
+  ));
 
-    return (
-      <ValidationContext.Consumer>
-        {ctx => (
-          <>
-            <div
-              className={classSet({
-                "c-form-field": true,
-                "m-error": hasError(this.props.field, ctx.error)
-              })}
-            >
-              {!!this.props.label && <label htmlFor={`input-${this.props.field}`}>{this.props.label}</label>}
-              <div className="c-form-field-wrapper">
-                <select
-                  id={`input-${this.props.field}`}
-                  defaultValue={this.props.defaultValue}
-                  onChange={this.onChange}
-                >
-                  {options}
-                </select>
-              </div>
-              <DisplayError fields={[this.props.field]} error={ctx.error} />
-              {this.props.children}
-            </div>
-          </>
-        )}
-      </ValidationContext.Consumer>
-    );
-  }
-}
+  return (
+    <ValidationContext.Consumer>
+      {(ctx) => (
+        <div
+          className={classSet({
+            "c-form-field": true,
+            "m-error": hasError(props.field, ctx.error),
+          })}
+        >
+          {!!props.label && <label htmlFor={`input-${props.field}`}>{props.label}</label>}
+          <div className="c-form-field-wrapper">
+            <select id={`input-${props.field}`} defaultValue={props.defaultValue} onChange={onChange}>
+              {options}
+            </select>
+          </div>
+          <DisplayError fields={[props.field]} error={ctx.error} />
+          {props.children}
+        </div>
+      )}
+    </ValidationContext.Consumer>
+  );
+};
