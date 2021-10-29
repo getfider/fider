0fc6d63fcb96cfe4244fabf78bb7cb7a7ce5e28d
Guilherme Oenning <me@goenning.net>
2021-10-29 06:46:34 +0100

    chore: update npm packages (#1023)

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,9 +22,10 @@ module.exports = {
     // Include .prettierrc.js rules
     "prettier/prettier": ["error", {}, { usePrettierrc: true }],
     "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { varsIgnorePattern: "_" }],
     "react/prop-types": "off",
-    "react/jsx-no-target-blank": ["error", { "allowReferrer": true }],
+    "react/jsx-no-target-blank": ["error", { allowReferrer: true }],
     "@typescript-eslint/no-explicit-any": "off", // TODO: turn this on in future
-    "@typescript-eslint/explicit-module-boundary-types": "off" // TODO: turn this on in future
+    "@typescript-eslint/explicit-module-boundary-types": "off", // TODO: turn this on in future
   },
-};
+}
