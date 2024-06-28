35ef53e052cac3b0b37f19e9a935726f07c8f325
Matt Roberts <roberts.mattroberts@gmail.com>
2024-06-28 14:15:25 +0100

    Only include email when listing the members.

--- app/models/entity/user.go
+++ app/models/entity/user.go
@@ -1,13 +1,17 @@
 package entity
 
-import "github.com/getfider/fider/app/models/enum"
+import (
+	"encoding/json"
+
+	"github.com/getfider/fider/app/models/enum"
+)
 
 // User represents an user inside our application
 type User struct {
 	ID            int             `json:"id"`
 	Name          string          `json:"name"`
 	Tenant        *Tenant         `json:"-"`
-	Email         string          `json:"email"`
+	Email         string          `json:"-"`
 	Role          enum.Role       `json:"role"`
 	Providers     []*UserProvider `json:"-"`
 	AvatarBlobKey string          `json:"-"`
@@ -40,4 +44,20 @@ func (u *User) IsAdministrator() bool {
 type UserProvider struct {
 	Name string
 	UID  string
+}
+
+// UserWithEmail is a wrapper around User that includes the email field when marshaling to JSON
+type UserWithEmail struct {
+	*User
+}
+
+func (umc UserWithEmail) MarshalJSON() ([]byte, error) {
+	type Alias User // Prevent recursion
+	return json.Marshal(&struct {
+		*Alias
+		Email string `json:"email"`
+	}{
+		Alias: (*Alias)(umc.User),
+		Email: umc.User.Email,
+	})
 }
