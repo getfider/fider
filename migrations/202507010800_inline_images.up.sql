aaaab5290e7a365c8da90186ea62a3ee87435293
Matt Roberts <roberts.mattroberts@gmail.com>
2025-07-01 10:37:48 +0100

    Better migration script for multiple attachments

--- migrations/202507010800_inline_images.up.sql
+++ migrations/202507010800_inline_images.up.sql
@@ -1,19 +1,31 @@
--- Update comments with attachment images
+-- Update comments with all attachment images
 UPDATE comments 
 SET content = CASE
-    WHEN content IS NULL OR content = '' THEN '![](fider_image:' || a.attachment_bkey || ')'
-    ELSE content || E'\n\n![](fider_image:' || a.attachment_bkey || ')'
+    WHEN content IS NULL OR content = '' THEN image_markdown
+    ELSE content || E'\n\n' || image_markdown
 END
-FROM attachments a
-WHERE comments.id = a.comment_id 
-AND a.comment_id IS NOT NULL;
+FROM (
+    SELECT 
+        comment_id,
+        string_agg('![](fider-image:' || attachment_bkey || ')', E'\n') as image_markdown
+    FROM attachments 
+    WHERE comment_id IS NOT NULL
+    GROUP BY comment_id
+) a
+WHERE comments.id = a.comment_id;
 
--- Update posts with attachment images  
+-- Update posts with all attachment images  
 UPDATE posts
 SET description = CASE
-    WHEN description IS NULL OR description = '' THEN '![](fider_image:' || a.attachment_bkey || ')'
-    ELSE description || E'\n\n![](fider_image:' || a.attachment_bkey || ')'
+    WHEN description IS NULL OR description = '' THEN image_markdown
+    ELSE description || E'\n\n' || image_markdown
 END
-FROM attachments a
-WHERE posts.id = a.post_id
-AND a.comment_id IS NULL;
+FROM (
+    SELECT 
+        post_id,
+        string_agg('![](fider-image:' || attachment_bkey || ')', E'\n') as image_markdown
+    FROM attachments 
+    WHERE comment_id IS NULL
+    GROUP BY post_id
+) a
+WHERE posts.id = a.post_id;
