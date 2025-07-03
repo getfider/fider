bd8d4399742730425f3a4037a18c7aeb3b466337
Matt Roberts <roberts.mattroberts@gmail.com>
2025-07-03 08:53:17 +0100

    Better icons

--- public/pages/Home/components/PostsSort.tsx
+++ public/pages/Home/components/PostsSort.tsx
@@ -5,7 +5,7 @@ import { i18n } from "@lingui/core"
 import IconSparkles from "@fider/assets/images/heroicons-sparkles-outline.svg"
 import IconThumbsUp from "@fider/assets/images/heroicons-thumbsup.svg"
 import IconChat from "@fider/assets/images/heroicons-chat-alt-2.svg"
-import IconInbox from "@fider/assets/images/heroicons-inbox.svg"
+import IconClock from "@fider/assets/images/heroicons-clock.svg"
 
 interface PostsSortProps {
   value: string
@@ -17,7 +17,7 @@ export const PostsSort: React.FC<PostsSortProps> = ({ value = "trending", onChan
     { value: "trending", label: i18n._("home.postfilter.option.trending", { message: "Trending" }), icon: IconSparkles },
     { value: "most-wanted", label: i18n._("home.postfilter.option.mostwanted", { message: "Most Wanted" }), icon: IconThumbsUp },
     { value: "most-discussed", label: i18n._("home.postfilter.option.mostdiscussed", { message: "Most Discussed" }), icon: IconChat },
-    { value: "recent", label: i18n._("home.postfilter.option.recent", { message: "Recent" }), icon: IconInbox },
+    { value: "recent", label: i18n._("home.postfilter.option.recent", { message: "Recent" }), icon: IconClock },
   ]
 
   const selectedItem = options.find((x) => x.value === value) || options[0]
