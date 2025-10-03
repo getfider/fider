8c73978399e21bc2fb01bf88acd5baa1e3c96a56
Matt Roberts <roberts.mattroberts@gmail.com>
2025-10-03 12:03:06 +0100

    Don't serve stuff for non-commercial users

--- app/handlers/moderation.go
+++ app/handlers/moderation.go
@@ -1,33 +1,28 @@
 package handlers
 
 import (
-	"net/http"
-
+	"github.com/getfider/fider/app"
 	"github.com/getfider/fider/app/pkg/web"
 )
 
 // ModerationPage is the moderation administration page (stub - requires commercial license)
 func ModerationPage() web.HandlerFunc {
 	return func(c *web.Context) error {
-		return c.Page(http.StatusOK, web.Props{
-			Page:  "Administration/pages/ContentModeration.page",
-			Title: "Moderation · Site Settings",
-		})
+		return c.Failure(app.ErrCommercialLicenseRequired)
 	}
 }
 
 // GetModerationItems returns all unmoderated posts and comments (stub - requires commercial license)
 func GetModerationItems() web.HandlerFunc {
 	return func(c *web.Context) error {
-		return c.Ok(web.Map{
-			"items": []interface{}{},
-		})
+		return c.Failure(app.ErrCommercialLicenseRequired)
 	}
 }
 
 // GetModerationCount returns the count of items awaiting moderation (stub - requires commercial license)
 func GetModerationCount() web.HandlerFunc {
 	return func(c *web.Context) error {
+		// Return 0 instead of error to allow UI to function normally
 		return c.Ok(web.Map{
 			"count": 0,
 		})
