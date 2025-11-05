5d656696161ee48e98d4f3d4cbc1a48df32faa3c
Matt Roberts <roberts.mattroberts@gmail.com>
2025-11-05 13:24:00 +0000

    Tidy up the content moderation scripts.

--- migrations/202507010831_add_is_approved.sql
+++
@@ -1,9 +0,0 @@
--- Add is_approved column to posts table
-ALTER TABLE posts ADD is_approved BOOLEAN NULL;
-UPDATE posts SET is_approved = true;
-ALTER TABLE posts ALTER COLUMN is_approved SET NOT NULL;
-
--- Add is_approved column to comments table  
-ALTER TABLE comments ADD is_approved BOOLEAN NULL;
-UPDATE comments SET is_approved = true;
-ALTER TABLE comments ALTER COLUMN is_approved SET NOT NULL;
