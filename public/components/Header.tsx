5d08b7a1fe0a4e8eaf2e240331d0bd3c4acba6de
Matt Roberts <roberts.mattroberts@gmail.com>
2025-12-12 20:18:21 +0000

    Mobile fixes for the content moderation indicator.

--- public/components/Header.tsx
+++ public/components/Header.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react"
-import { SignInModal, RSSModal, TenantLogo, NotificationIndicator, ModerationIndicator, UserMenu, ThemeSwitcher, Icon, Button } from "@fider/components"
+import { SignInModal, RSSModal, TenantLogo, NotificationIndicator, UserMenu, ThemeSwitcher, Icon, Button, ModerationIndicator } from "@fider/components"
 import { useFider } from "@fider/hooks"
 import { HStack } from "./layout"
 import { Trans } from "@lingui/react/macro"
 import { i18n } from "@lingui/core"
 import IconRss from "@fider/assets/images/heroicons-rss.svg"
+import "./Header.scss"
 
 interface HeaderProps {
   hasInert?: boolean
@@ -33,15 +34,14 @@ export const Header = (props: HeaderProps) => {
       <SignInModal isOpen={isSignInModalOpen} onClose={hideSignInModal} />
       <RSSModal isOpen={isRSSModalOpen} onClose={hideRSSModal} url={`${fider.settings.baseURL}/feed/global.atom`} />
       <HStack className="c-menu p-4 w-full">
-        <div className="container">
-          <HStack justify="between">
-            <a href="/" className="flex flex-x flex-items-center flex--spacing-2 h-8">
-              <TenantLogo size={100} />
-              <h1 className="text-header">{fider.session.tenant.name}</h1>
-            </a>
-            {fider.session.isAuthenticated && (
-              <>
-                <ModerationIndicator />
+        <div className="container c-header__container">
+          <div className="flex flex-wrap flex-items-center gap-2">
+            <div className="flex flex-x flex-items-center justify-between w-full">
+              <a href="/" className="flex flex-x flex-items-center flex--spacing-2 h-8">
+                <TenantLogo size={100} />
+                <h1 className="text-header">{fider.session.tenant.name}</h1>
+              </a>
+              {fider.session.isAuthenticated && (
                 <HStack spacing={2}>
                   {fider.session.tenant.isFeedEnabled && (
                     <button title={atomFeedTitle} className="c-themeswitcher" onClick={showRSSModal}>
@@ -52,26 +52,31 @@ export const Header = (props: HeaderProps) => {
                   <NotificationIndicator />
                   <UserMenu />
                 </HStack>
-              </>
+              )}
+              {!fider.session.isAuthenticated && (
+                <HStack spacing={2}>
+                  {fider.session.tenant.isFeedEnabled && (
+                    <button title={atomFeedTitle} className="c-themeswitcher" onClick={showRSSModal}>
+                      <Icon sprite={IconRss} className="h-6 text-gray-500" />
+                    </button>
+                  )}
+                  <ThemeSwitcher />
+                  <Button variant="primary" size="default" onClick={handleSignInClick}>
+                    <HStack spacing={1} className="flex-items-center">
+                      <span>
+                        <Trans id="action.signin">Sign in</Trans>
+                      </span>
+                    </HStack>
+                  </Button>
+                </HStack>
+              )}
+            </div>
+            {fider.session.isAuthenticated && (
+              <div className="c-header__moderation">
+                <ModerationIndicator />
+              </div>
             )}
-            {!fider.session.isAuthenticated && (
-              <HStack spacing={2}>
-                {fider.session.tenant.isFeedEnabled && (
-                  <button title={atomFeedTitle} className="c-themeswitcher" onClick={showRSSModal}>
-                    <Icon sprite={IconRss} className="h-6 text-gray-500" />
-                  </button>
-                )}
-                <ThemeSwitcher />
-                <Button variant="primary" size="default" onClick={handleSignInClick}>
-                  <HStack spacing={1} className="flex-items-center">
-                    <span>
-                      <Trans id="action.signin">Sign in</Trans>
-                    </span>
-                  </HStack>
-                </Button>
-              </HStack>
-            )}
-          </HStack>
+          </div>
         </div>
       </HStack>
     </div>
