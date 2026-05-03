0a4a4de5fb8469e47614df7775dd11c238ea86b8
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-05-03 18:25:44 -0400

    spelling: original
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- app/actions/post.go
+++ app/actions/post.go
@@ -264,8 +264,8 @@ func (action *SetResponse) Validate(ctx context.Context, user *entity.User) *val
 			result.AddFieldFailure("originalNumber", i18n.T(ctx, "validation.custom.selfduplicate"))
 		}
 
-		getOriginaPost := &query.GetPostByNumber{Number: action.OriginalNumber}
-		err := bus.Dispatch(ctx, getOriginaPost)
+		getOriginalPost := &query.GetPostByNumber{Number: action.OriginalNumber}
+		err := bus.Dispatch(ctx, getOriginalPost)
 		if err != nil {
 			if errors.Cause(err) == app.ErrNotFound {
 				result.AddFieldFailure("originalNumber", i18n.T(ctx, "validation.custom.originalpostnotfound"))
@@ -274,8 +274,8 @@ func (action *SetResponse) Validate(ctx context.Context, user *entity.User) *val
 			}
 		}
 
-		if getOriginaPost.Result != nil {
-			action.Original = getOriginaPost.Result
+		if getOriginalPost.Result != nil {
+			action.Original = getOriginalPost.Result
 		}
 	}
 
